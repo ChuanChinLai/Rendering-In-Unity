2e2a5018590682572ddafb877f28fbf8b96eb359
ChuanChinLai <chuanchinlai@gmail.com>
2018-04-15 00:32:24 -0600

    working on 4x4matrix

---
+++ Rendering-In-Unity/Assets/Scripts/ScaleTransformation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleTransformation : Transformation
+{
+    public Vector3 vec3 = Vector3.one;
+
+    public override Vector3 Apply(Vector3 point)
+    {
+        point.x *= vec3.x;
+        point.y *= vec3.y;
+        point.z *= vec3.z;
+
+        return point;
+    }
+}
---
+++ Rendering-In-Unity/Assets/Scripts/TranslationTransformation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslationTransformation : Transformation
+{
+    public Vector3 vec3;
+
+    public override Vector3 Apply(Vector3 point)
+    {
+        return point + vec3;
+    }
+}
